de6b760e0d2442ca2fb4908e1b96f91c11147535
Luth-DWN <luth.dwn@gmail.com>
2022-06-02 09:37:55 +0700

    Create Layout

---
+++ src/components/Layout/Rightbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-navigation-drawer v-model="drawer" clipped app right permanent>
+      <v-list class="ml-4 mr-4 mt-6">
+          <h3>Ranking User</h3>
+        <v-list-item v-for="name in links" :key="name.name" link>
+          <v-list-item-content>
+            <v-list-item-title>{{name.id}}.{{ name.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name:'RightBar',
+  data: () => ({
+    drawer: true,
+    clipped: true,
+    fixed: true,
+    links: [{name:'jance', id:'1'}, {name:'janve',id:'2'}, {name:'janeg',id:'3'}, {name:'janet',id:'4'}, {name:'janre',id:'5'}, {name:'janse',id:'6'}, {name:'janed',id:'7'}],
+  }),
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,30 @@
 <template>
-  <hello-world />
+  <v-app>
+
+    <hello-world />
+    <header-view />
+    <left-bar />
+    <right-bar />
+
+
+
+  </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import HeaderView from "../components/Layout/Header";
+import LeftBar from "../components/Layout/Leftbar.vue";
+import RightBar from "../components/Layout/Rightbar.vue";
 
-  export default {
-    name: 'HomeView',
+export default {
+  name: "HomeView",
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    HelloWorld,
+    HeaderView,
+    LeftBar,
+    RightBar,
+  },
+};
 </script>
