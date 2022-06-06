e6d9fe56b920a38b20369bedb7b6c67a1d45f3fe
Luth-DWN <luth.dwn@gmail.com>
2022-06-06 20:45:27 +0700

    View Login and Regist

--- fgd-22/layouts/default.vue
+++ fgd-22/layouts/default.vue
@@ -1,118 +1,51 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
     <v-app-bar
+      color="white"
       :clipped-left="clipped"
-      fixed
+      :clipped-right="clipped"
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn text disabled>
+        <img src="img/logo.svg" height="50" />
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
+      <v-spacer></v-spacer>
+      <v-responsive max-width="500">
+        <v-text-field
+          label="Search"
+          dense
+          flat
+          elevation="5"
+          hide-details
+          solo-inverted
+        >
+        </v-text-field>
+      </v-responsive>
+      <v-spacer></v-spacer>
+
+      <v-btn to="/login" depressed color="primary" class="rounded-xl mr-5">
+        Login
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
+
+      <v-btn to="/regist" rounded depressed color="error" class="rounded-xl"
+        >Register</v-btn
       >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+  name: "DefaultLayout",
+  data: () => ({
+    drawer: true,
+    clipped: true,
+    fixed: true,
+  }),
+};
 </script>
---
+++ fgd-22/pages/login.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+      <v-col cols="8" sm="6">
+        <v-card class="elevation-0 mt-12">
+          <v-window>
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" md="12">
+                  <v-card-text class="mt">
+                    <v-row justify="center">
+                      <v-col cols="12" sm="6">
+                        <v-img
+                          class="mx-auto"
+                          max-width="150px"
+                          src="img/welcome.svg"
+                        ></v-img>
+                        <v-img
+                          class="mx-auto"
+                          max-width="100px"
+                          max-height="50px"
+                          src="img/logo.svg"
+                        ></v-img>
+                        <v-text-field
+                          label="Email"
+                          outlined
+                          dense
+                          :rules="emailRules"
+                          color="blue"
+                          autocomplete="false"
+                          class="mt-16"
+                        />
+
+                        <v-text-field
+                          label="Password"
+                          outlined
+                          dense
+                          :rules="passRules"
+                          color="blue"
+                          autocomplete="false"
+                          type="password"
+                        />
+
+                        <v-row>
+                          <v-col cols="12" sm="7"> </v-col>
+                        </v-row>
+
+                        <v-btn color="primary" dark block elevation="5"
+                          >Log in</v-btn
+                        >
+
+                        <v-col cols="6" sm="12">
+                          <span class="caption blue--text"
+                            >Forgot password</span
+                          >
+                        </v-col>
+
+                        <h5 class="text-center grey--text mt-4 mb-3">
+                          Don't have a moot account?
+                        </h5>
+
+                        <router-link to="/regist">
+                          <v-btn color="error" dark block>Register</v-btn>
+                        </router-link>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+  name: "LoginPage",
+  data: () => ({
+    valid: true,
+    name: "",
+    passRules: [
+      (v) => !!v || "Password is require",
+      (v) => (v && v.length >= 8) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "email can not be empty",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    checkbox: false,
+  }),
+};
+</script>
+
+<style scoped>
+.v-application .rounded-bl-xl {
+  border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+  border-bottom-right-radius: 300px !important;
+}
+</style>
