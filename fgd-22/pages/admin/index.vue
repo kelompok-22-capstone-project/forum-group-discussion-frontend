7c1d3e1a9924604089a469e0dc368c338a096d82
Luth-DWN <luth.dwn@gmail.com>
2022-06-13 09:46:28 +0700

    Index Page Dashboard

--- fgd-22/pages/admin/index.vue
+++ fgd-22/pages/admin/index.vue
@@ -1,13 +1,72 @@
 <template>
-  <div>
-    Admin !!!!! Required login
-  </div>
+  <v-div class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col md="6">
+        <v-card class="ma-8 pa-6 rounded-lg" color="primary" link>
+          <v-list-item three-line class="white--text">
+            <v-list-item-content>
+              <div class="mb-2">Total User</div>
+              <v-list-item-title class="text-h5 mb-1"> 190 </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar size="80">
+              <v-icon size="74" color="#ffff">mdi-account</v-icon>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col md="6">
+        <v-card class="ma-8 pa-6 rounded-lg" color="primary" link>
+          <v-list-item three-line class="white--text">
+            <v-list-item-content>
+              <div class="mb-2">Total Threads</div>
+              <v-list-item-title class="text-h5 mb-1"> 190 </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar size="80">
+              <v-icon size="74" color="#ffff">mdi-note-outline</v-icon>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col md="6">
+        <v-card class="ma-8 pa-6 rounded-lg" color="error" link>
+          <v-list-item three-line class="white--text">
+            <v-list-item-content>
+              <div class="mb-2">Total Moderator</div>
+              <v-list-item-title class="text-h5 mb-1"> 190 </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar size="80">
+              <v-icon size="74" color="#ffff">mdi-account-edit</v-icon>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col md="6">
+        <v-card class="ma-8 pa-6 rounded-lg" color="error" link>
+          <v-list-item three-line class="white--text">
+            <v-list-item-content>
+              <div class="mb-2">Total Report</div>
+              <v-list-item-title class="text-h5 mb-1"> 190 </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar size="80">
+              <v-icon size="74" color="#ffff">mdi-alert-circle-outline</v-icon>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-div>
 </template>
 
 <script>
 export default {
-  middleware: 'auth'
-}
+  layout: "admin",
+};
 </script>
 
-<style lang="css" scoped></style>
+<style scoped></style>
